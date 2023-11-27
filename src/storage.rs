fb9c8b94d31f8cc0b1581db7537539df04384afb
Andrei Sudovtsov <asudovtsov@gmail.com>
2023-11-27 09:41:56 +0300

    Use unchecked methods

--- src/storage.rs
+++ src/storage.rs
@@ -96,8 +96,7 @@ impl Storage {
             self.data
                 .entry(TypeId::of::<T>())
                 .or_insert(Bucket::new::<T>())
-                .try_place(self.capacity, data)
-                .unwrap()
+                .place_unchecked(self.capacity, data)
         };
 
         Tid::new(index, cycle)
@@ -109,8 +108,7 @@ impl Storage {
             self.data
                 .entry(type_id)
                 .or_insert(Bucket::new::<T>())
-                .try_place(self.capacity, data)
-                .unwrap()
+                .place_unchecked(self.capacity, data)
         };
 
         Id::new(index, cycle, type_id)
@@ -120,7 +118,7 @@ impl Storage {
         match id.as_tid() {
             Some(tid) => match self.data.get_mut(&TypeId::of::<T>()) {
                 Some(bucket) if tid.index < bucket.len() => unsafe {
-                    bucket.try_remove(self.capacity, tid.index)
+                    bucket.remove_unchecked(self.capacity, tid.index)
                 },
                 _ => None,
             },
@@ -132,7 +130,7 @@ impl Storage {
         match id.as_tid() {
             Some(tid) => match self.data.get(&TypeId::of::<T>()) {
                 Some(bucket) if tid.index < bucket.len() => unsafe {
-                    bucket.try_get(self.capacity, tid.index)
+                    bucket.get_ucnhecked(self.capacity, tid.index)
                 },
                 _ => None,
             },
@@ -144,7 +142,7 @@ impl Storage {
         match id.as_tid() {
             Some(tid) => match self.data.get_mut(&TypeId::of::<T>()) {
                 Some(bucket) if tid.index < bucket.len() => unsafe {
-                    bucket.try_get_mut(self.capacity, tid.index)
+                    bucket.get_mut_unchecked(self.capacity, tid.index)
                 },
                 _ => None,
             },
@@ -197,64 +195,5 @@ impl Drop for Storage {
         for (_, bucket) in self.data.iter_mut() {
             unsafe { Bucket::drop(bucket, self.capacity) }
         }
-    }
-}
-
-// #[derive(Debug)]
-// struct A {
-//     string: String,
-// }
-
-// impl Drop for A {
-//     fn drop(&mut self) {
-//         println!("###Drop A {:?}", self);
-//     }
-// }
-
-mod tests {
-    #[test]
-    fn place_remove_contains() {
-        use super::*;
-
-        type Color = (String, u8, u8, u8);
-        // struct Label(String);
-
-        let mut storage = Storage::new();
-        let red = storage.place((String::from("red"), 255, 0, 0));
-        let green = storage.place((String::from("green"), 0, 255, 0));
-
-        for i in 0..10_000_000 {
-            let index = storage.place::<Color>((String::from("red"), 255, 0, 0));
-            // storage.remove(&index);
-            if i % 10_000_000 - 1 == 0 {
-                println!(
-                    "! {} {:?} {} {}",
-                    i,
-                    index,
-                    storage.contains(&index),
-                    storage.dead_cells_count::<Color>()
-                );
-            }
-            let id = storage.place_id::<Color>((String::from("red"), 255, 0, 0));
-            storage.contains_id(&id);
-        }
-
-        let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf);
-
-        // let blue = storage.place((String::from("blue"), 0, 0, 255));
-        // let label = storage.place(Label("label".into()));
-        // println!("{:?}\n{:?}\n{:?}", red, green, blue);
-        // println!(
-        //     "{:?}\n{:?}\n{:?}",
-        //     storage.contains(&red),
-        //     storage.contains(&green),
-        //     storage.contains(&blue)
-        // );
-
-        // assert!(storage.contains::<Color>(&red));
-        // assert!(storage.contains::<Color>(&green));
-        // assert!(storage.contains::<Color>(&blue));
-        // assert!(storage.contains::<Label>(&label));
     }
 }
